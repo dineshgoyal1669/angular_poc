741778fcbed22562a270ac5912e295776a803fcd
Dinesh Goyal <dinesh.goyal@quovantis.com>
2016-09-01 11:57:05 +0530

    logic for total cost

--- controller/items_controller.js
+++ controller/items_controller.js
@@ -3,6 +3,8 @@ angular.module('cart')
 	$scope.filters = {};
 	$scope.items = '';
 	$scope.favourites = [];
+	$scope.cart = [];
+	$scope.total_cost = 0;
 	ItemListService.getItems().then(function successCallback(response){
 		$scope.items = response.data;
 	});
@@ -29,5 +31,36 @@ angular.module('cart')
 			return false;
 		}
 	}
+	$scope.decrementCount = function(item){
+		if(item.count != 0){
+			item.count = Number(item.count)-1;
+			if($scope.cart.indexOf(item)==-1){
+				$scope.cart.push(item);
+			}else{
+				$scope.cart[$scope.cart.indexOf(item)].count = item.count;
+			}
+		}else{
+			var index = $scope.cart.indexOf(item);
+  			$scope.cart.splice(index, 1); 
+		}
+		calculateCost();
+	}
+	$scope.incrementCount = function(item){
+		item.count = Number(item.count)+1;
+		console.log(item.count);
+		if($scope.cart.indexOf(item)==-1){
+			$scope.cart.push(item);
+		}else{
+			$scope.cart[$scope.cart.indexOf(item)].count = item.count;
+		}
+		calculateCost();
+	}
+	var calculateCost = function(){
+		$scope.total_cost = 0;
+		for (i = 0; i < $scope.cart.length; i++) { 
+			console.log($scope.cart[i]);
+			$scope.total_cost += $scope.cart[i].count * $scope.cart[i].cost;
+		}
+	}	
 
 }]);
